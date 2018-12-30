54c0f03ee33b9f0d74eecd9fccbac2bb26d9cc79
Tobias Schürg <tobiasschuerg@gmail.com>
2018-12-30 11:36:58 +0100

    SDK to 28 and migration to AndroidX and using https.

--- app/src/main/java/com/tobiasschuerg/telekom/MainActivity.kt
+++ app/src/main/java/com/tobiasschuerg/telekom/MainActivity.kt
@@ -3,7 +3,7 @@ package com.tobiasschuerg.telekom
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.tobiasschuerg.telekom.service.TelekomService
 import com.tobiasschuerg.telekom.service.TelekomServiceImpl
 import com.tobiasschuerg.telekom.service.data.Status
--- app/src/main/java/com/tobiasschuerg/telekom/backend/TelekomApi.kt
+++ app/src/main/java/com/tobiasschuerg/telekom/backend/TelekomApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
 interface TelekomApi {
 
     companion object {
-        const val API_BASE_URL = "http://pass.telekom.de/api/service/generic/v1/"
+        const val API_BASE_URL = "https://pass.telekom.de/api/service/generic/v1/"
     }
 
     @GET("status")
